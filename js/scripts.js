3875f2184f8a3b12c47d8983c762aa4bc1bbd37f
Alexandre Ouellette <alexandre.f.ouellette@gmail.com>
2017-06-20 11:57:43 -0400

    Modified contact section and cleaned up CSS/JS

--- js/scripts.js
+++ js/scripts.js
@@ -11,32 +11,6 @@
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 
-//Google Maps
-var map;
-function initMap(){
-  var myCoords = {lat:45.4214, lng:-75.6919};
-
-  map = new google.maps.Map(document.getElementById('gmap'),{
-    center: myCoords,
-    zoom: 11
-  });
-
-  var image = {
-    url: 'img/icons/my-8bit-face-40.png',
-    size: new google.maps.Size(40, 40),
-    origin: new google.maps.Point(0,0),
-    anchor: new google.maps.Point(0,0)
-  };
-
-  var marker = new google.maps.Marker({
-    position: {lat:45.4532473, lng:-75.6550977},
-    map: map,
-    icon: image,
-    title: "Here I am!",
-    animation: google.maps.Animation.DROP,
-  });
-};
-
 $(document).ready(function() {
   // Smooth scrolling
   var $root = $('html, body');
@@ -53,66 +27,4 @@ $(document).ready(function() {
   //Stellar
   $.stellar();
 
-  //Tooltips
-  $(function () {
-    $('#bilingual-tooltip').tooltip();
-    $('#contact-tooltip').tooltip();
-    $('#writing-tooltip').tooltip();
-    $('[data-toggle="tooltip"]').tooltip();
-  });
-
-  // Character count for message box
-  $(".message-box").on('keyup', function () {
-
-    console.log("Keyup event triggered...");
-
-    // Get character count
-    var charCount = $(".message-box").val().length;
-
-    // Log character count
-    console.log(charCount);
-
-    // Append to the DOM
-    $('#char-count').html("Character count: " + charCount);
-
-    // Change color if longer than 50 characters
-    if(charCount > 50) {
-      $("#char-count").css("color", "red");
-    } else {
-      $("#char-count").css("color", "white");
-    };
-  });
-
-  // Behavior on contact form submit
-  $("#contact-form-button").on("click", function() {
-    var name = $("#input-name").val();
-    var phoneNumber = $("#input-tel").val();
-    var email = $("#input-email").val();
-    var message = $("#input-message").val();
-
-    if(message == "") {
-      $("#input-message").css("border", "2px solid red");
-    } else {
-
-      validationMessage = "Thanks for your message " + name + "! <br>" + "Here is what you sent us:<br><br> <u>Email:</u> " + email + "<br>" + "<u>Phone Number:</u> " + phoneNumber + "<br>" + "<u>Message:</u><br>" + message;
-
-      $("#visible-comment").html(validationMessage);
-
-      $(".contact-form-labels").hide();
-      $("#input-name").hide();
-      $("#input-tel").hide();
-      $("#input-email").hide();
-      $(".message-box").hide();
-    };
-
-    return false;
-  });
-
-  $(".item")
-    .mouseenter( function() {
-      $(".info", this).show();
-    })
-    .mouseleave( function() {
-      $(".info", this).hide();
-    });
 });
